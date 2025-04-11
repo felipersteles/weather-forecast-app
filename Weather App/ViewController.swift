e259362c973d037719ce24c8cba59aa1f87c58f0
Felipe Teles <felipersteles@gmail.com>
2025-04-11 13:15:26 -0300

    Initial Commit

---
+++ Weather App/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Felipe Teles on 11/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.systemBlue;
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+    
+    OV
+}
+
